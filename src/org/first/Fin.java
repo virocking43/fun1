a96655fed4321e2131bd2e6a78b376ebc9773b86
virocking43 <vignesh.waran43@yahoo.com>
2022-09-14 12:26:05 +0530

    sucessful addition

--- src/org/first/Fin.java
+++ src/org/first/Fin.java
@@ -1,5 +1,30 @@
 package org.first;
 
 public class Fin {
+private void sys() {
+	// TODO Auto-generated method stub
+System.out.println("new addition");
+}
+
+private void sys() {
+	// TODO Auto-generated method stub
+System.out.println("new addition");
+}
+private void sys() {
+	// TODO Auto-generated method stub
+System.out.println("new addition");
+}
+private void sys() {
+	// TODO Auto-generated method stub
+System.out.println("new addition");
+}
+private void sys() {
+	// TODO Auto-generated method stub
+System.out.println("new addition");
+}
+private void sys() {
+	// TODO Auto-generated method stub
+System.out.println("new addition");
+}
 
 }
